7877691ba41b8453e037c312b43f5ada16941623
kensuke kobayashi <kensuke02248@gmail.com>
2026-05-06 23:51:29 +0900

    uplaod

---
+++ モジュール.py
@@ -0,0 +1,8 @@
+prompt = int(input("プロンプトを選択してください。"))
+if prompt == 1:
+    # randomモジュール
+    import random
+
+    omikuji = ["大吉", "中吉", "小吉", "凶", "大凶"]
+    result = random.choice(omikuji)
+    print(f"今日の運勢は{result}です。")
