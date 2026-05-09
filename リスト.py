62dfe999e540f7fd9d405d4738024d90d180d3d9
kensuke kobayashi <kensuke02248@gmail.com>
2026-05-09 22:51:50 +0900

    upload

--- リスト.py
+++ リスト.py
@@ -61,3 +61,54 @@ if prompt == 7:
 
     for name, price in zip(names, prices):
         print(name, price)
+
+if prompt == 8:
+    wd = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+    print(wd)
+    print(wd[1])
+
+if prompt == 9:
+    todo = ["買い物"]
+    todo.append("掃除")  # ["買い物", "掃除"]
+    todo.insert(0, "散歩")  # ["散歩", "買い物", "掃除"] (0番目に割り込み)
+    print(todo)
+
+if prompt == 10:
+    colors = ["赤", "青", "黄"]
+    colors[1] = "緑"  # 1番目（青）を「緑」に上書き
+    # 結果: ["赤", "緑", "黄"]
+    print(colors)
+
+if prompt == 11:
+    drinks = ["紅茶", "コーヒー", "ミルク"]
+    last_item = drinks.pop()  # ミルクを取り出して削除
+    drinks.remove("紅茶")  # 紅茶を取り出して削除
+    print(drinks)
+
+if prompt == 12:
+    japanese = ["い", "う", "あ", "え", "お"]
+    japanese.sort()
+    """sort(): 中身を小さい順（あいうえお順）に並べ替える。"""
+    print(japanese)
+
+if prompt == 13:
+    number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    number.reverse()
+    """reverse(): 今の並びを真逆にひっくり返す。"""
+    print(number)
+
+if prompt == 14:
+    number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    number.reverse()
+    """index(値): 指定した値が「何番目にあるか」を調べる。"""
+    print(number.index(5))
+
+if prompt == 15:
+    wd = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+    sorted(wd, reverse=True)
+    print(wd)
+
+if prompt == 16:
+    wd = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+    sorted(wd, key=lambda x: len(x))
+    print(wd)
